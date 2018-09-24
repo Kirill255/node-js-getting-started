cc04bd2d233d65251b5e1ab22da9542543fa9227
Kirill255 <jhgbnm1@mail.ru>
2018-09-25 00:33:28 +0300

    Add times route and set config var TIMES

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('pages/index'))
+	.get('/cool', (req, res) => res.send(cool()))
+	.get('/times', (req, res) => {
+		let result = ''
+		const times = process.env.TIMES || 5
+		for (i = 0; i < times; i++) {
+			result += i + ' '
+		}
+		res.send(result)
+	})
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
